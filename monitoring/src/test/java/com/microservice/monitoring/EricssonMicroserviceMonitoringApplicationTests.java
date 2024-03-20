38f2412774afca600af71cad99b63f88d319f738
MinaAsghari <mina.asghari1808@gmail.com>
2024-03-20 18:21:05 +0100

    creat and add springboot application

---
+++ monitoring/src/test/java/com/microservice/monitoring/EricssonMicroserviceMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricssonMicroserviceMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
